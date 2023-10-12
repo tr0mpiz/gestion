764db0572167dff0a16f118b500055eb8dc22806
tr0mpiz <nicolaspiersanti@hotmail.com>
2023-10-12 10:08:43 -0300

    aaaa

--- src/public/js/funcionesHtmlSocios.js
+++ src/public/js/funcionesHtmlSocios.js
@@ -347,89 +347,93 @@ idtarea.addEventListener('change', async () =>{
     
     $('[data-bs-toggle="tooltip"]').tooltip();
     
-    $('input[type="checkbox"]').on('click', function () {
-      const id = $(this).data('id'); // Obtén el ID del registro desde el atributo `data-id`
-      const estado = this.checked ? 1 : 0; // Cambia el estado según si el checkbox está marcado o desmarcado
-      const campo = $(this).data('campo'); // Obtén el valor del atributo `data-campo`
-      //que oculte el modal
-      $('#tareasModal').modal('hide');
-      // Muestra una SweetAlert para confirmar la acción del usuario
-      Swal.fire({
-        title: '¿Estás seguro?',
-        text: 'Esta acción no se puede deshacer',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Sí, confirmar',
-        cancelButtonText: 'Cancelar'
-      }).then((result) => {
-        if (result.isConfirmed) {
-          // El usuario confirmó la acción, realiza la solicitud AJAX al servidor
-          $.ajax({
-            url: '/produccion/cambiaestado?id=' + id + '&estado=' + estado + '&campo=' + campo,
-            method: 'POST',
-            success: function (response) {
-              // Maneja la respuesta del servidor, si es necesario
-              console.log(response);
+    // Asignar un manejador de eventos al elemento DataTable que no cambia
+$('#dataTableTareas').on('click', 'input[type="checkbox"]', function () {
+  const id = $(this).data('id'); // Obtén el ID del registro desde el atributo `data-id`
+  const estado = this.checked ? 1 : 0; // Cambia el estado según si el checkbox está marcado o desmarcado
+  const campo = $(this).data('campo'); // Obtén el valor del atributo `data-campo`
+  //que oculte el modal
+  $('#tareasModal').modal('hide');
+  // Muestra una SweetAlert para confirmar la acción del usuario
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: 'Esta acción no se puede deshacer',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, confirmar',
+    cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      // El usuario confirmó la acción, realiza la solicitud AJAX al servidor
+      $.ajax({
+        url: '/produccion/cambiaestado?id=' + id + '&estado=' + estado + '&campo=' + campo,
+        method: 'POST',
+        success: function (response) {
+          // Maneja la respuesta del servidor, si es necesario
+          console.log(response);
+      
+          $('input[type="checkbox"]').on('click', function () {
+            const id = $(this).data('id'); // Obtén el ID del registro desde el atributo `data-id`
+            const estado = this.checked ? 1 : 0; // Cambia el estado según si el checkbox está marcado o desmarcado
+            const campo = $(this).data('campo'); // Obtén el valor del atributo `data-campo`
           
-              $('input[type="checkbox"]').on('click', function () {
-                const id = $(this).data('id'); // Obtén el ID del registro desde el atributo `data-id`
-                const estado = this.checked ? 1 : 0; // Cambia el estado según si el checkbox está marcado o desmarcado
-                const campo = $(this).data('campo'); // Obtén el valor del atributo `data-campo`
-              
-                // Muestra una SweetAlert para confirmar la acción del usuario
-                Swal.fire({
-                  title: '¿Estás seguro?',
-                  text: 'Esta acción no se puede deshacer',
-                  icon: 'warning',
-                  showCancelButton: true,
-                  confirmButtonColor: '#3085d6',
-                  cancelButtonColor: '#d33',
-                  confirmButtonText: 'Sí, confirmar',
-                  cancelButtonText: 'Cancelar'
-                }).then((result) => {
-                  if (result.isConfirmed) {
-                    // El usuario confirmó la acción, realiza la solicitud AJAX al servidor
-                    $.ajax({
-                      url: '/produccion/cambiaestado?id=' + id + '&estado=' + estado + '&campo=' + campo,
-                      method: 'POST',
-                      success: function (response) {
-                        // Maneja la respuesta del servidor, si es necesario
-                        console.log(response);
-                        //que muestre el modal
-                        
+            // Muestra una SweetAlert para confirmar la acción del usuario
+            Swal.fire({
+              title: '¿Estás seguro?',
+              text: 'Esta acción no se puede deshacer',
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Sí, confirmar',
+              cancelButtonText: 'Cancelar'
+            }).then((result) => {
+              if (result.isConfirmed) {
+                // El usuario confirmó la acción, realiza la solicitud AJAX al servidor
+                $.ajax({
+                  url: '/produccion/cambiaestado?id=' + id + '&estado=' + estado + '&campo=' + campo,
+                  method: 'POST',
+                  success: function (response) {
+                    // Maneja la respuesta del servidor, si es necesario
+                    console.log(response);
+                    //que muestre el modal
+                    
 
+                
                     
-                        
-                    },
-                    
-                      error: function (xhr, textStatus, errorThrown) {
-                        // Maneja errores si es necesario
-                        console.log(textStatus);
-                      }
-                    });
-                  } else {
-                    
-                    // El usuario canceló la acción, desmarca el checkbox
-                    $(this).prop('checked', !this.checked);
-                    
+                },
+                
+                  error: function (xhr, textStatus, errorThrown) {
+                    // Maneja errores si es necesario
+                    console.log(textStatus);
                   }
                 });
-              });
-          },
-          
-            error: function (xhr, textStatus, errorThrown) {
-              // Maneja errores si es necesario
-              console.log(textStatus);
-            }
+              } else {
+                
+                // El usuario canceló la acción, desmarca el checkbox
+                $(this).prop('checked', !this.checked);
+                
+              }
+            });
           });
-        } else {
-          // El usuario canceló la acción, desmarca el checkbox
-          $(this).prop('checked', !this.checked);
+      },
+      
+        error: function (xhr, textStatus, errorThrown) {
+          // Maneja errores si es necesario
+          console.log(textStatus);
         }
       });
-    });
+    } else {
+      // El usuario canceló la acción, desmarca el checkbox
+      $(this).prop('checked', !this.checked);
+    }
+  });
+});
+
+
+
 
           
     
